bea951c5adfaac9728f0cbe3dbdd7a3b17096654
ramo-szn <anuarramo@gmail.com>
2023-05-20 13:33:38 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,17 +5,12 @@
  */
 int main(void)
 {
-	putchar(48);
-	putchar(49);
-	putchar(50);
-	putchar(51);
-	putchar(52);
-	putchar(53);
-	putchar(54);
-	putchar(55);
-	putchar(56);
-	putchar(57);
+	int i;
 
+	for (i = 0; i < 10; i++)
+	{
+	putchar(i + '0');
+	}
 	putchar('\n');
 	return (0);
 }
