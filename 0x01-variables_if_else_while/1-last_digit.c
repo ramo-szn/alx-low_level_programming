6ebeb2d1b0b3ff6adb554e11aa44b3bf6695f223
ramo-szn <anuarramo@gmail.com>
2023-05-20 08:00:28 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ int main(void)
 
 	if (lastdigit > 5)
 	{
-		printf("and is greater than 5\n");
+		printf("%d and is greater than 5\n," lastdigit);
 	}
 	else if (lastdigit == 0)
 	{
-		printf("and is 0\n");
+		printf("%d and is 0\n", lastdigit);
 	}
 	else
 	{
-		printf("and is less than 6 and not 0\n");
+		printf("%d and is less than 6 and not 0\n", last digit);
 	}
 	return (0);
 }
